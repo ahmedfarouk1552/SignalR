922f130e683446f96eb03c1538abe54d1b104039
Ahmed Abdou <ahmedabdou1552@gmail.com>
2021-12-22 15:13:16 +0200

    SignalR

--- SingalrChat/ChatHub.cs
+++ SingalrChat/ChatHub.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using SingalrChat.Models;
+using System.Threading.Tasks;
 
 namespace SingalrChat
 {
@@ -12,14 +13,50 @@ namespace SingalrChat
     public class ChatHub : Hub
     {
         ChatContext db = new ChatContext();
-        public void sendmessage(string name,string mess)
+        public void sendmessage(string name,string mess )
         {
             Clients.All.newmessage(name, mess);
-
-            message m =new message() { name=name,message1=mess,date=DateTime.Now}; 
+            message m = new message() { name = name, message1 = mess, date = DateTime.Now };
             db.messages.Add(m);
             db.SaveChanges();
-            
+
+            //Clients.All.newmessage(m);
+
+
+
+
+        }
+
+        public override Task OnConnected()
+        {
+            //string id = Context.ConnectionId;
+            return base.OnConnected();  
+        }
+
+        public void joingroup(string groupName,string name)
+        {
+            Groups.Add(Context.ConnectionId, groupName);
+            try
+            {
+                Clients.OthersInGroup(groupName).newmember(name, groupName);
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        public void sendtogroup(string groupname, string name, string message)
+        {
+
+            try
+            {
+                Clients.Group(groupname).messagegroup(groupname, name, message);
+            }
+            catch (Exception)
+            {
+
+            }
         }
     }
 }
